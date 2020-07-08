cfb6537491d5e0a524fd3cc9b9fddc599d400cd4
adeelgeeksquad <development.geeksquad@gmail.com>
2020-07-08 18:12:53 +0500

    boxes created in dashboard page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react'
 import Header from './components/header/header'
 import './assets/style/style.css'
 import Navbar from './components/navbar/navbar'
-
+import Dashboard from './components/dashboard/dashboard'
 export default()=>{
   return(
     <>
       <Header/>
       <Navbar/>
+      <Dashboard/>
     </>
   );
 }
